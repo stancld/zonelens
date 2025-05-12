8b6e5ff9fed559382bacf4ec89273a269a7dce35
stancld <daniel.stancl@gmail.com>
2025-05-12 14:22:41 +0200

    Add command to generate cert files

--- backend/tools/run_https_server.sh
+++ backend/tools/run_https_server.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# You can generate cert files with:
+# ```bash
+# openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes
+# ```
 python manage.py runserver_plus --cert-file cert.pem --key-file key.pem
