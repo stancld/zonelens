89580152af1ba73b9c6170c5a9efce6e734d4bd3
stancld <daniel.stancl@gmail.com>
2025-06-05 10:27:58 +0200

    backend.tools: Remove unused scripts

--- backend/tools/process_activities.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-USER_STRAVA_ID=$1
-curl -X POST -H "Content-Type: application/json" -d '{"user_strava_id": $1, "after_timestamp": "2025-03-01T00:00:00Z"}' http://127.0.0.1:8000/api/process-activities/
