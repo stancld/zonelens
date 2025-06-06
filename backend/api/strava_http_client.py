03096118797954ebec734226f7264f1c53ed4c28
stancld <daniel.stancl@gmail.com>
2025-06-06 10:33:58 +0200

    backend: Clean strava_http_client.py

--- backend/api/strava_http_client.py
+++ backend/api/strava_http_client.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2025 Dan Stancl
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
@@ -23,12 +45,8 @@ class StravaHttpClient:
 			STRAVA_PUSH_SUBSCRIPTIONS_URL,
 			params={"client_id": client_id, "client_secret": client_secret},
 		)
-		self.logger.info(f"Status Code: {response.status_code}")
-		try:
-			return response.json()
-		except Exception as e:
-			self.logger.error(f"Failed to get subscription: {e}")
-			raise
+		response.raise_for_status()
+		return response.json()
 
 	def register_subscription(
 		self, client_id: str | int, client_secret: str, callback_url: str, verify_token: str
@@ -42,16 +60,12 @@ class StravaHttpClient:
 				"verify_token": verify_token,
 			},
 		)
-		self.logger.info(f"Status Code: {response.status_code}")
-		try:
-			return response.json()
-		except Exception as e:
-			self.logger.error(f"Failed to register subscription: {e}")
-			raise
+		response.raise_for_status()
+		return response.json()
 
 	def delete_subscription(
 		self, client_id: str | int, client_secret: str, subscription_id: str
-	) -> dict[str, Any]:
+	) -> None:
 		response = requests.delete(
 			STRAVA_PUSH_SUBSCRIPTIONS_URL,
 			params={
@@ -60,9 +74,4 @@ class StravaHttpClient:
 				"subscription_id": subscription_id,
 			},
 		)
-		self.logger.info(f"Status Code: {response.status_code}")
-		try:
-			return response.json()
-		except Exception as e:
-			self.logger.error(f"Failed to delete subscription: {e}")
-			raise
+		response.raise_for_status()
