598e5a780f8d778274fbc7869e64ef5b846afb96
stancld <daniel.stancl@gmail.com>
2025-06-22 14:22:24 +0200

    extension: Add background job to display Changelog page upon extension update

---
+++ extension/background.js
@@ -0,0 +1,10 @@
+const PRODUCTION_DOMAIN = 'https://strava-zones.com';
+const DEVELOPMENT_DOMAIN = 'https://localhost:8000';
+const IS_PRODUCTION_BUILD = false; // Set to true for production builds
+
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason === 'update') {
+        console.log(`Zonelens updated to ${chrome.runtime.getManifest().version}.`);
+        chrome.tabs.create({ url: `${IS_PRODUCTION_BUILD ? PRODUCTION_DOMAIN : DEVELOPMENT_DOMAIN}/api/changelog/` });
+    }
+});
